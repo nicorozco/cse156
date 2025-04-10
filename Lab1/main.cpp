7dc74c897e0def3fb36c5f2b906c59fc7ff74d52
nicorozco <84752428+nicorozco@users.noreply.github.com>
2025-04-10 18:24:43 +0000

    Add files via upload
    latest

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector> 
 #include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -8,7 +7,7 @@
 #include <unistd.h>      // for close()
 #include <cstring>       // for memset()
 #include <errno.h>       // for errno
-
+#include <cstdlib> // for stoi()
 
 
 
@@ -17,11 +16,10 @@ int main (int argc, char* argv[]) {
 	
 	std::string hostname;
 	std::string url;
-	//int port = 0;
 	char h_option;
 	std::string serverIdentifier;
 	std::string ip_address;
-	
+	std::string httpRequest;
 
 	if (argc < 2){
 			std::cout << "Please enter the correct format for the program: ./main hostname (-H for headers only) IP Adress:Port/path to document" << "\n";
@@ -40,9 +38,9 @@ int main (int argc, char* argv[]) {
 
                 // Step 2: Find '/' after the colon (split port and path)
                 size_t slashPos = url.find('/', colonPos);
-                std::string port = url.substr(colonPos + 1, slashPos - colonPos - 1);
+		std::string port = url.substr(colonPos + 1, slashPos - colonPos - 1);
                 std::string path = url.substr(slashPos); // from slash to end
-
+		int portInt = std::stoi(port);
 		
 		// Prints for Troubleshooting:
 		std::cout << "Number of Arguments Provided:  "<< argc << "\n";
@@ -72,21 +70,62 @@ int main (int argc, char* argv[]) {
 		serverAddress.sin_family = AF_INET;
 		// 2c.) set the port number
 		//	utilize htons to ensure big endian
-	        //serverAddress.sin_port = htons(port); 
+	        serverAddress.sin_port = htons(portInt); 
 		// 2d.) set the IP Address
 		// we utilize (inet_pton) a function to convert the IP address from text form into binary
 		// since we are utilizing a function we are going to pass the ip variable by reference of the structure
 		//inet expects a constant string type, type cast the string in to a pointer
-		inet_pton(AF_INET,ip_address.c_str(),&serverAddress.sin_addr);
 		
-		// 3.) Connect to the server (.connect())
-		// 4.) Send the request (.request())
-		// 5.) recived the request (.recv())
-		//  5a.) if the h option is set only give the headers
-		//
-		if (h_option){
-			std::cout << "Sending HEAD METHOD into packet" << "\n";
+		if (inet_pton(AF_INET,ip_address.c_str(),&serverAddress.sin_addr) <= 0 ){
+		// check for errors: 1 means address was set succesfuly, anything less than 1 means error
+			std::cerr << "Invalid Address / Address not Supported" << "\n";
+			return -1;
 		}
+		// 3.)Intiate a TCP connection to the server (.connect())
+		// what does .connect() do?
+		// connect intiates a tcp connection through the socket to the server address (being the socket structure of the server) 
+		if (connect(clientSocket,(struct sockaddr*)&serverAddress,sizeof(serverAddress)) < 0){
+		// check for different error:
+		// if connections was sucessful connect returns 0, if error return -1
+		// error message is stored in errno
+			switch(errno) {
+				case ETIMEDOUT:
+				// client tcp connection intiatiation recieves no response 
+					std::cerr << "Error: Connection timed out (ETIMEDOUT)" << "\n";	
+					break;
+				case ECONNREFUSED:
+					std::cerr << "Error: Connection refused (ECONNREFUSED)" << "\n";	
+					break;
+				//server sends reset, refusing to connect to client
+				case EHOSTUNREACH:
+				//unable to reach the host
+					std::cerr << "Error: Host Unreachable  (EHOSTUNREACH)" << "\n";	
+					break;
+				case ENETUNREACH:
+				// unable to reach the network
+					std::cerr << "Error: Network Unreachable (ENETUNREACH)" << "\n";	
+					break;
+				default:
+					std::cerr << "Error: " << strerror(errno) << "\n";
+					break;
+			}
+		}
+		// 4.) Send the request (.request())
+		// HTTP Request Line Format:
+		// 3 Entires
+		// 1.) Method: GET/HEAD
+		// 2.) URL : url provided by client
+		// 3.) Version: we will use HTTP/1.1
+		if (h_option){
+		//  4a.) if the h option is set the http request METHOD TO head
+			httpRequest = "GET" + path + "HTTP/1,1" + "\n";
+			std::cout << httpRequest << "\n";
+		} 
+		// 4b.) else use the regualar GET METHOD
+		
+		//  5b.) else reverse the respose and write into a file
+		// 5.) recived the request (.recv())
+		//
 		//  5b.) else reverse the respose and write into a file
 		// 5b.)
 		// 6.) Close the socket (.close())
