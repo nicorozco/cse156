118c98f74965ed9fdb0a9e0612f2f6ee1e788cdf
nicorozco <84752428+nicorozco@users.noreply.github.com>
2025-04-13 18:13:58 +0000

    Add files via upload
    latest

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -32,32 +32,28 @@ int main (int argc, char* argv[]) {
 	}
 	
 	// check if the flag is provided 
-	if(std::string(argv[1]) == "-h"){
-		if (argc < 4) {
-			std::cerr << "Error: Missing hostname or URL.\n";
-			return 1;
-		}
+	if (argc == 4 && std::string(argv[3]) == "-h") {
 		hOption = true;
-		option = argv[1];
-		hostname  = argv[2];
-		url = argv[3];
-	}else{
+		hostname  = argv[1];
+		url = argv[2];
+	}else if (argc == 3){
 		//no -h option given
 	 	hostname = argv[1];
 	 	url = argv[2];	
+	} else {
+		std::cerr << "Usage: ./main <hostname> <IP Address:Port Number(Optional)> <-h (for headers only)>" << "\n";
+
 	}
-		//split the url into multiple parts 
-		//1.) IP 2.) Port 3.) Document Path
-
-	        // Step 1: Find ':' (split IP and port)
-    	        size_t colonPos = url.find(':');
-                std::string ip = url.substr(0, colonPos);
-
-                // Step 2: Find '/' after the colon (split port and path)
-                size_t slashPos = url.find('/', colonPos);
-		std::string port = url.substr(colonPos + 1, slashPos - colonPos - 1);
-                std::string path = url.substr(slashPos); // from slash to end
-		int portInt = std::stoi(port);
+	//split the url into multiple parts 
+	//1.) IP 2.) Port 3.) Document Path
+	// Step 1: Find ':' (split IP and port)
+    	size_t colonPos = url.find(':');
+        std::string ip = url.substr(0, colonPos);
+	// Step 2: Find '/' after the colon (split port and path)
+        size_t slashPos = url.find('/', colonPos);
+	std::string port = url.substr(colonPos + 1, slashPos - colonPos - 1);
+        std::string path = url.substr(slashPos); // from slash to end
+	int portInt = std::stoi(port);
 		
 		// Prints for Troubleshooting:
 		//std::cout << "Number of Arguments Provided:  "<< argc << "\n";
@@ -71,29 +67,36 @@ int main (int argc, char* argv[]) {
 
 		
 
-		//std::cout << "Creating Socket" << "\n";
-		// 1.) Create a Socket(file descriptor)	
-		int clientSocket = socket(AF_INET,SOCK_STREAM,0);
-		//error has occured creating socket 
-		if (clientSocket < 0) {
-			std::cerr << "Socket creating failed: " <<strerror(errno) << "\n";
-			return 1;
-		}	
-		// 2.) Specify the Server Address we utilize a structure for the address
-		sockaddr_in serverAddress; 
-		// 2b.) set the IP Family
-		serverAddress.sin_family = AF_INET;
-		// 2c.) set the port number
-		//utilize htons to ensure big endian
-	        serverAddress.sin_port = htons(portInt); 
-		// 2d.) set the IP Address
-		// we utilize (inet_pton) a function to convert the IP address from text form into binary	
+	//std::cout << "Creating Socket" << "\n";
+	// 1.) Create a Socket(file descriptor)	
+	int clientSocket = socket(AF_INET,SOCK_STREAM,0);
+	//error has occured creating socket 
+	if (clientSocket < 0) {
+		std::cerr << "Socket creating failed: " << strerror(errno) << "\n";
+		return 1;
+	}	
+	// 2.) Specify the Server Address we utilize a structure for the address
+	sockaddr_in serverAddress; 
+	// 2b.) set the IP Family
+	serverAddress.sin_family = AF_INET;
+	// 2c.) set the port number
+	//utilize htons to ensure big endian
+	//checking if port is valid
+	if (portInt < 1 || portInt > 65535){
+		std::cerr << "Error: Invalid port number\n";
+		return 6;
+		
+	}
+	serverAddress.sin_port = htons(portInt); 
+	// 2d.) set the IP Address
+	// we utilize (inet_pton) a function to convert the IP address from text form into binary	
 		//std::cout << "IP Address: " << ip<< "\n";
-		if (inet_pton(AF_INET,ip.c_str(),&serverAddress.sin_addr) < 0 ){
+	if (inet_pton(AF_INET,ip.c_str(),&serverAddress.sin_addr) < 0 ){
 		// check for errors: 1 means address was set succesfuly, anything less than 1 means error
-			std::cerr << "Invalid Address / Address not Supported" << "\n";
-			return -1;
-		}
+		std::cerr << "Invalid Address / Address not Supported" << "\n";
+		close(clientSocket);
+		return 7;
+	}
 		// 3.)Intiate a TCP connection to the server (.connect())
 		//std::cout << "Server IP Address: " << inet_ntoa(serverAddress.sin_addr) << "\n";
 		//std::cout << "Server Port: " << ntohs( serverAddress.sin_port) << "\n";
@@ -101,30 +104,32 @@ int main (int argc, char* argv[]) {
 		// connect intiates a tcp connection through the socket to the server address (being the socket structure of the server) 
 		//std::cout << "Initiating TCP Connection" << "\n";
 		
-		if (connect(clientSocket,(struct sockaddr*)&serverAddress,sizeof(serverAddress)) < 0){
+	if (connect(clientSocket,(struct sockaddr*)&serverAddress,sizeof(serverAddress)) < 0){
 		// check for different error:
 		// if connections was sucessful connect returns 0, if error return -1
 		// error message is stored in errno
-			switch(errno) {
+		close(clientSocket);
+		switch(errno) {
 				// client tcp connection intiatiation recieves no response 
-				case ETIMEDOUT:
-					std::cerr << "Error: Connection timed out (ETIMEDOUT)" << "\n";	
-					break;
-				//server sends reset, refusing to connect to client
-				case ECONNREFUSED:
-					std::cerr << "Error: Connection refused (ECONNREFUSED)" << "\n";	
-					break;
-				//unable to reach the host
-				case EHOSTUNREACH:
-					std::cerr << "Error: Host Unreachable  (EHOSTUNREACH)" << "\n";	
-					break;
+			case ETIMEDOUT:
+				std::cerr << "Error: Connection timed out (ETIMEDOUT)" << "\n";	
+				break;
+			//server sends reset, refusing to connect to client
+			case ECONNREFUSED:
+				std::cerr << "Error: Connection refused (ECONNREFUSED)" << "\n";	
+				break;
+			//unable to reach the host
+			case EHOSTUNREACH:
+				std::cerr << "Error: Host Unreachable  (EHOSTUNREACH)" << "\n";	
+				break;
+				return 7;
 				// unable to reach the network
-				case ENETUNREACH:
-					std::cerr << "Error: Network Unreachable (ENETUNREACH)" << "\n";	
-					break;
-				default:
-					std::cerr << "Error: " << strerror(errno) << "\n";
-					break;
+			case ENETUNREACH:
+				std::cerr << "Error: Network Unreachable (ENETUNREACH)" << "\n";	
+				break;
+			default:
+				std::cerr << "Error: " << strerror(errno) << "\n";
+				break;
 			}
 		}
 		// 4.) Send the request (.request())
@@ -145,7 +150,10 @@ int main (int argc, char* argv[]) {
 		//std::cout << "Sending Get Request" << "\n";
 		httpRequest = "GET " + path + " HTTP/1.1\r\n" + "Host: " + hostname + "\r\n" + "Connection: close\r\n" + "\r\n";
 		// the second argument is a pointer to where you want to store the response
-		send(clientSocket, httpRequest.c_str(), httpRequest.length(), 0);
+		if ((send(clientSocket, httpRequest.c_str(), httpRequest.length(), 0)) < 1){
+			std::cerr << "Error Sending HTTP Request" << "\n";
+			close(clientSocket);
+		}
 		
 
 		// 5.) recived the request (.recv())
@@ -156,6 +164,7 @@ int main (int argc, char* argv[]) {
 			// while we are recieving write the buffer into the file 
 			fullData.insert(fullData.end(), buffer, buffer + bytesrecv);	
 			if(hOption){
+				//h option write out to terminal
 				std::cout.write(buffer,bytesrecv);
 			}
 		}
@@ -170,18 +179,18 @@ int main (int argc, char* argv[]) {
 
 		if (bytesrecv < 0){
 			std::cerr << "Error recieving" << "\n";
+			close(clientSocket);
 		}
-		std::cout << "\n";
-
 		
 		if (hOption == false){
 			//utilize reverse algorithm to reverse the data
 			std::reverse(fullData.begin(), fullData.end());
 			//create a file 
-			std::ofstream outfile("output.txt");
+			std::ofstream outfile("slug_download_norozco6.dat");
 			//check for error openning file
 			if (!outfile) {
 				std::cerr << "Failed to open file for writing" << "\n";
+				close(clientSocket);
 			}else{
 				//write from the fulldata input into the file
 				//std::cout << "Writing Final Data to File" << "\n";
